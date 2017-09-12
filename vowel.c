d3cf8f8412b86358e7dcf6309cc050bb34b4b346
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2017-09-12 16:48:08 +0530

    Update vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main
+{
+int z;
+printf("enter the number");
+scanf("%d"&z);
+if (z=a||z=e||z=i||z=o||z=e)
+{
+printf("z is vowel");
+else
+{
+printf("z is not vowel ");
+}
+}
+}
