568c19cf4721a90e032d99a6bdd2c8ebed95f85e
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-01-11 16:57:25 +0530

    Create larg.c

---
+++ larg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a= 1,2,3;
+scanf("%d",&a);
+if(1<2&&1<3||2>1&&3>2)
+{
+printf(" 3 is a largest number");
+}
+else
+{
+printf(" 3 is an not largest number");
+}
+}
+
