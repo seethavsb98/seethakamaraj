c7c9dfcd572e137130582bec9c1b0df5bb3d554a
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-01-11 16:56:40 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char c;
+prin tf("enter a character:  ");
+scanf("%c",&c);
+if(c>='a'&& c<='z'&&)||(c>='A'&&c<='z'))
+{
+printf("%c is an alphabets.",c);
+}
+else
+{
+printf("%c is  not an alphabets.",c);
+}
+return 0;
+}
