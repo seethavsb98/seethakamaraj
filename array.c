78f50785b8afb42a9f0fcaa4fc137e7823059a4b
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-01-18 16:07:39 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int N,K;
+int array N[10]={0,1,2,3,4};
+int array k[10]={5,6,7,8,9};
+scanf("%d%d",&N&k);
+printf("sum of the array number");
+add(sum)
+{
+ sum=N+K;
+  }
+printf(" ",&sum);
+}
