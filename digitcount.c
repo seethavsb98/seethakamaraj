1ec57bb1b1298f90264ddacca0d10257468ab72e
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-01-21 11:44:18 +0530

    Create digitcount.c

---
+++ digitcount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int countDigits;
+    int counter;
+    countDigits=0;
+    printf("Enter a string: ");
+    gets(str);
+    for(counter=0;str[counter]!=NULL;counter++)
+    {
+    if(str[counter]>='0' && str[counter]<='9')
+            countDigits++;
+     }
+    printf("\nDigits:%d",countdigits);
+    return 0;
+}
