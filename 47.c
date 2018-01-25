ebdc1f22baa85f7d8bbd03ca50d37fc6731fd6e7
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-01-25 10:49:34 +0000

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,i
+array a[i]={1,2,3,4,5,6,7,8,9,10};
+scanf("%d%d",&a,&i);
+printf("enter the number");
+if(a[i]<5)
+{
+a[i]=small;
+printf("enter the smallest number");
+}
+if(a[i]>5)
+{
+printf("enter the largest number");
+}
+}
