472feef3dc4f9b8f0fa4057dcb4abd068f5b5116
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-01-25 10:59:52 +0000

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i=1,a=4,c;
+scanf("%d%d",&i,7,a);
+printf("enter the number",i);
+for(i=o;i<6;i++)
+{
+printf("enter the number");
+if(a%2)
+{
+printf(" ",&c);
+c++;
+printf("print the number",&c);
+}
+}
+}
