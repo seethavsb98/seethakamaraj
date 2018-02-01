583b735de1ef77a442c8caaa2964acd1043587ab
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-02-01 10:53:39 +0000

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum=0;
+printf("enter the number");
+scanf("%d",&a);
+while(a!=0)
+{
+  b=a%10;
+  sum=(sum*10)+b;
+  a=a/10;
+}
+while(sum!=0)
+{
+  switch(sum%10)
+  {
+    case 0:
+       printf("zero");
+       break;
+   case 1:
+       printf("one");
+       break;
+   case 2:
+       printf("two");
+       break;
+  case 3:
+       printf("three");
+       break;
+   case 4:
+       printf("four");
+       break;
+    case 5:
+       printf("five");
+       break;
+     case 6:
+       printf("six");
+       break;
+    case 7:
+       printf("seven");
+       break;
+    case 8:
+       printf("eight");
+       break;
+        case 9:
+       printf("nine");
+       break;
+  }
+  sum=sum/10;
+}
+}
