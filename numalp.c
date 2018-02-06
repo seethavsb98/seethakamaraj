3e176d6b820e5817197b5ccb95c2bef445b37780
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-02-06 09:25:43 +0000

    Create numalp.c

---
+++ numalp.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+int main() {
+char a[100];
+int i;
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+  if(a[i]>='a'||a[i]<='z'||a[i]>='A'||a[i]<='Z'&&a[i]>=0||a[i]<=9)
+  {
+    printf("yes");
+    break;
+  }
+}
+}
