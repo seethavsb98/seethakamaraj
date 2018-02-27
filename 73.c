0eb6a592c4da27e5b792a8b0505dae82c89e29bc
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-02-27 15:59:37 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,l,r;
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the left and right number");
+scanf("%d%d",&l,&r);
+if((n>l)&&(n<r))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return o;
+}
