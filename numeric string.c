6a74bc311461d5f390a31dec4f9e69b5ae47578c
seethavsb98 <31504431+seethavsb98@users.noreply.github.com>
2018-03-15 16:28:00 +0530

    Create numeric string.c

---
+++ numeric string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char s[100];
+int i;
+scanf("%s",a);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]>=0||s[i]<=9)
+{
+printf("\n");
+}
+else
+{
+break;
+}
+}
+printf("\nnumeric");
+}
